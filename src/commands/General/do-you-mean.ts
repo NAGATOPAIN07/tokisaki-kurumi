df428d29283ce72f9c13aa09006af6ed4d5e064f
Examinati <72595444+Eximinati@users.noreply.github.com>
2022-04-07 19:34:56 +0500

    Update do-you-mean.ts

--- src/commands/General/do-you-mean.ts
+++ src/commands/General/do-you-mean.ts
@@ -14,13 +14,13 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         const n = [
-            'https://telegra.ph/file/c1894127580460d188b3b.mp4'
+            'https://c.tenor.com/X8rNHwat3IYAAAPo/date-a-live-kurumi-tokisaki.mp4'
         ]
         let lucy = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: lucy }, MessageType.video, {
             quoted: M.WAMessage,
             mimetype: Mimetype.gif,
-            caption: `𝙄𝙩'𝙨 𝙩𝙤𝙤 𝙦𝙪𝙞𝙚𝙩 *${M.sender.username}!* 𝙔𝙤𝙪 𝙢𝙚𝙖𝙣 🐈*${this.client.config.prefix}help*? \n` }
+            caption: 'baka try *${M.sender.username}!* 𝙔𝙤𝙪 𝙢𝙚𝙖𝙣 🐈*${this.client.config.prefix}help*? \n` }
         )
     }
 }
