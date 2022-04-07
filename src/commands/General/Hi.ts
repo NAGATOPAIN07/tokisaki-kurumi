321d91099639029f668c36055d4afd1032a1b483
Examinati <Aliwadood561@gmail.com>
2022-04-07 17:47:18 +0500

    update

---
+++ src/commands/General/Hi.ts
@@ -0,0 +1,37 @@
+/** @format */
+
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { ISimplifiedMessage } from "../../typings";
+
+export default class Command extends BaseCommand {
+  constructor(client: WAClient, handler: MessageHandler) {
+    super(client, handler, {
+      command: "hi",
+      description: "Generally used to check if bot is Up",
+      category: "general",
+      usage: `${client.config.prefix}hi`,
+      baseXp: 10,
+    });
+  }
+
+  run = async (M: ISimplifiedMessage): Promise<void> => {
+    const buttons = [
+      {
+        buttonId: "help",
+        buttonText: { displayText: `${this.client.config.prefix}help` },
+        type: 1,
+      },
+    ];
+
+    const buttonMessage: any = {
+      contentText: `Hello Beautiful 😍,🅴🅻🅰🅸🅽🅰 Bot present here👋`,
+      footerText: "💜 🅴🅻🅰🅸🅽🅰 💜",
+      buttons: buttons,
+      headerType: 1,
+    };
+    await M.reply(buttonMessage, MessageType.buttonsMessage);
+  };
+}
