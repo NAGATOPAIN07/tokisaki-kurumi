cb97f811538679ac5f8a5bbf54a444b66ca7281a
Examinati <72595444+Eximinati@users.noreply.github.com>
2022-04-07 19:47:16 +0500

    Update Savage.ts

--- src/commands/General/Savage.ts
+++ src/commands/General/Savage.ts
@@ -17,23 +17,21 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         const n = [
-            'https://telegra.ph/file/8eed1568a71b3b55dac05.jpg','https://telegra.ph/file/6777fe4846a3b0af4decd.jpg'
+            'https://i.pinimg.com/564x/37/f1/42/37f142bfbfc1bcc7be1b104e4dbf6af4.jpg','https://i.pinimg.com/564x/0f/c3/7c/0fc37c10eee0e70e918202e1fa084fa9.jpg'
         ]
         let rin = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: rin }, MessageType.image, {quoted:M.WAMessage,
             mimetype: Mimetype.jpeg,
-            caption: `🍭𝗵𝗲𝗹𝗹𝗼!🍃I'm Lord Savage 🕊️ a learner/student & an upcoming developer in the future.
+            caption: `🍭𝗵𝗲𝗹𝗹𝗼!🍃I'm AKU, just a regular ugly guy.
             
 🍁𝙒𝙝𝙖𝙩𝙨𝘼𝙥𝙥;
-Wa.me/+263716928420
-Wa.me/+263716564623
+Wa.me/+923087880256
 
 📮𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢;
-https://instagram.com/_smokey_vines_
-https://instagram.com/amapiano_state
+｟𝖢𝗈𝗆𝗂𝗇𝗀 𝖲𝗈𝗈𝗇｠
 
 🚀𝘿𝙞𝙨𝙘𝙤𝙧𝙙;
-｟𝖢𝗈𝗆𝗂𝗇𝗀 𝖲𝗈𝗈𝗇｠
+discordapp.com/users/Sempai#5552
 
 ⪼𝖲𝖾𝖾 𝗒𝖺𝗁 💘` }
         )
