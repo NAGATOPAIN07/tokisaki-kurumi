8de66c7360c4dab03fbe16acac88402dec385c31
Examinati <72595444+Eximinati@users.noreply.github.com>
2022-04-07 19:20:25 +0500

    Update report.ts

--- src/commands/General/report.ts
+++ src/commands/General/report.ts
@@ -26,9 +26,9 @@ export default class Command extends BaseCommand {
             await this.client.sendMessage(
                // enter your unique gid
 `263716564623-1628429288@g.us`,
-                `「 💙🅴🅻🅰🅸🅽🅰 🆁🅴🅿🅾🆁🆃💙 」\n\n ${term} by ${M.sender.username} \n
+                `「 💙🅺🆄🆁🆄🅼🅸 🆁🅴🅿🅾🆁🆃💙 」\n\n ${term} by ${M.sender.username} \n
                    From : ${M.groupMetadata?.subject} `,
                 MessageType.text
             );
-            return void M.reply('Sent the bot admin your report To Lord Savage 🕊️ if you used this command for fun you will be banned!!')
+            return void M.reply('Your report has been submitted to AKU, if you used this command for fun you will be banned!!')
         }}
